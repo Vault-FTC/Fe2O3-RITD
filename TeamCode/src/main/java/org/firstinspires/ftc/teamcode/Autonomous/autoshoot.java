4a645c272394705529db352e31e67391394963de
Wildcat09 <aleithea.sietman@gmail.com>
2025-10-21 19:10:02 -0400

    Code for Scrimmage #1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/autoshoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/autoshoot.java
@@ -21,8 +21,18 @@ public class autoshoot extends LinearOpMode {
     MotorSpeeds launchSpeed = MotorSpeeds.NEAR;
 
     Location firstPosition = new Location(-130, 0);
-    Location secondPosition = new Location(-130, 100);
 
+    //we need to turn here -45 degrees
+    Location secondPosition = new Location(-115,114);
+
+    //we need to prestart the motor to launch
+    Location thirdPosition = new Location(-115,-114);
+
+    //we need to turn here 45 degrees
+    //after we turn we need to turn on the feeder
+    Location fourthPosition = new Location(-115,100);
+
+    //this moves the robot out of the triangle
     LimeLight limelight;
 
     void setTargets()
@@ -54,42 +64,42 @@ public class autoshoot extends LinearOpMode {
             double turnVal = 0;
 
             LLResultTypes.FiducialResult result = limelight.getEitherResult();
-            if(result != null) {
+            if (result != null) {
                 turnVal = result.getTargetXDegrees() * 0.045;
             }
 
-            if(time < 6)
-            {
-
+            if (time < 3) {
+// the 3 was 6
                 driveallclass.driveToPosition(firstPosition, 0, telemetry);
-            }
-            else if (time < 12)
-            {
+            } else if (time < 6) {//the 6 was 12
                 driveallclass.drive(0, 0, turnVal);
                 armBase.setSpeed(launchSpeed);
                 armBase.setShooter(true);
-            }
-            else if (time < 14)
-            {
+            } else if (time < 7) {//the 7 was 14
                 armBase.setShooter(true);
                 intake.spinTransfer(1);
                 intake.spinIntake(1);
                 intake.spinKicker(0.4);
-            }
-            else if(time < 15)
-            {
+            } else if (time < 8) {//the 8 was 15
                 //armBase.setKicker(false);
                 intake.spinTransfer(0);
                 intake.spinIntake(0);
                 intake.spinKicker(0);
                 armBase.setShooter(false);
-            }
-            else if(time < 20)
-            {
+            } else if (time < 10) {//the 10 was 20
                 driveallclass.driveToPosition(secondPosition, 0, null);
-            }
-            else if(time < 22) {
+
+
+
+            } else if (time < 15) {
+
+                driveallclass.driveToPosition(thirdPosition, 0, telemetry);
+
+            } else if(time < 18) {// the 18 was 22
                 driveallclass.drive(0, 0, 0);
+
+            } else if (time < 24) {
+                driveallclass.driveToPosition(fourthPosition, 0, telemetry);
             }
 
             driveallclass.update();
